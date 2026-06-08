c15b88eaf8b0f6194e8e7df4527b92c62801e567
irxyad <irxyadjoka@gmail.com>
2026-06-08 19:41:04 +0800

    att2

--- packages/mason_templates/bloc/hooks/pre_gen.dart
+++ packages/mason_templates/bloc/hooks/pre_gen.dart
@@ -2,62 +2,31 @@ import 'dart:io';
 
 import 'package:mason/mason.dart';
 
-import '../models/bloc_config.dart';
-import 'utils/bloc_type_utils.dart';
-import 'utils/build_runner_utils.dart';
-import 'utils/check_existing_folder_utils.dart';
 import 'utils/error_utils.dart';
-import 'utils/resolve_output_dir_utils.dart';
-import 'utils/validate_name_utils.dart';
 
 Future<void> run(HookContext context) async {
   try {
-    final isCalledFromParent =
-        context.vars['called_from_parent'] as bool? ?? false;
+    // if (!isCalledFromParent) {
+    //   validateName(
+    //     context,
+    //     defaultValue: 'counter',
+    //     label: 'What is the bloc name? (e.g. auth, home, profile)',
+    //     hint: 'without bloc/cubit suffix',
+    //   );
+    // }
 
-    if (!isCalledFromParent) {
-      validateName(
-        context,
-        defaultValue: 'counter',
-        label: 'What is the bloc name? (e.g. auth, home, profile)',
-        hint: 'without bloc/cubit suffix',
-      );
-    }
+    // resolveOutputDir(context);
+    // _resolveBlocType(context);
+    // resolveBuildRunner(context);
 
-    resolveOutputDir(context);
-    _resolveBlocType(context);
-    resolveBuildRunner(context);
+    // // Semua vars sudah terset, baru parse config
+    // final config = BlocConfig.fromContext(context);
+    // final folderPath = '${config.outputDir}/${config.name}_${config.blocType}';
 
-    // Semua vars sudah terset, baru parse config
-    final config = BlocConfig.fromContext(context);
-    final folderPath = '${config.outputDir}/${config.name}_${config.blocType}';
-
-    checkExistingFolder(context, folderPath: folderPath);
+    // checkExistingFolder(context, folderPath: folderPath);
+    context.logger.info('TESTING');
   } catch (e) {
     context.logger.err('Generation Aborted! ${e.message}');
     exit(1);
   }
-}
-
-void _resolveBlocType(HookContext context) {
-  final argBlocType = context.vars['bloc_type'] as String?;
-
-  // Jika sudah ada, berarti dipanggil dari parent brick
-  if (argBlocType != null) {
-    final blocType = BlocType.fromString(argBlocType);
-    _setBlocTypeVars(context, blocType);
-    return;
-  }
-
-  final blocType = resolveBlocType(context);
-  _setBlocTypeVars(context, blocType);
-}
-
-void _setBlocTypeVars(HookContext context, BlocType blocType) {
-  context.vars = {
-    ...context.vars,
-    'bloc_type': blocType.value,
-    'isBloc': blocType == BlocType.bloc,
-    'isCubit': blocType == BlocType.cubit,
-  };
 }
