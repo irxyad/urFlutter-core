230c185e4f20c1d1a66d55e64e4623eebbdc6b2b
irxyad <irxyadjoka@gmail.com>
2026-06-08 19:53:01 +0800

    att5

--- packages/mason_templates/bloc/hooks/pre_gen.dart
+++ packages/mason_templates/bloc/hooks/pre_gen.dart
@@ -1,30 +1,23 @@
-import 'dart:io';
-
 import 'package:mason/mason.dart';
 
 void run(HookContext context) {
-  try {
-    // if (!isCalledFromParent) {
-    //   validateName(
-    //     context,
-    //     defaultValue: 'counter',
-    //     label: 'What is the bloc name? (e.g. auth, home, profile)',
-    //     hint: 'without bloc/cubit suffix',
-    //   );
-    // }
+  // if (!isCalledFromParent) {
+  //   validateName(
+  //     context,
+  //     defaultValue: 'counter',
+  //     label: 'What is the bloc name? (e.g. auth, home, profile)',
+  //     hint: 'without bloc/cubit suffix',
+  //   );
+  // }
 
-    // resolveOutputDir(context);
-    // _resolveBlocType(context);
-    // resolveBuildRunner(context);
+  // resolveOutputDir(context);
+  // _resolveBlocType(context);
+  // resolveBuildRunner(context);
 
-    // // Semua vars sudah terset, baru parse config
-    // final config = BlocConfig.fromContext(context);
-    // final folderPath = '${config.outputDir}/${config.name}_${config.blocType}';
+  // // Semua vars sudah terset, baru parse config
+  // final config = BlocConfig.fromContext(context);
+  // final folderPath = '${config.outputDir}/${config.name}_${config.blocType}';
 
-    // checkExistingFolder(context, folderPath: folderPath);
-    context.logger.info('TESTING');
-  } catch (e) {
-    context.logger.err('Generation Aborted! $e');
-    exit(1);
-  }
+  // checkExistingFolder(context, folderPath: folderPath);
+  context.logger.info('TESTING');
 }
